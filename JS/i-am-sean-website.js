18306023087886977af383b077bfdf4fb2c355ff
Sean-Beyer <61070387+Thalious@users.noreply.github.com>
2020-08-30 20:17:09 -0700

    Reuploading portfolio repo

---
+++ JS/i-am-sean-website.js
@@ -0,0 +1,27 @@
+// Smooth Scrolling
+
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+        if (this.Link !== "") {
+            event.preventDefault();
+            var Link = this.Link;
+
+            $('html, body').animate({
+                scrollTop: $(Link).offset().top
+            }, 2000, function(){
+
+                window.location.Link = Link; 
+            });
+        }
+    });
+});
+
+// Form functions
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
